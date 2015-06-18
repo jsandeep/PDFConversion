484c24a00c7e18b15fe2a96eba03044117c81b36
Siddharth Jain <c.siddharth.jain@uptake.com>
2015-06-18 17:11:37 -0500

    Modified the main PDF converter to use XML Worker

--- src/PdfConverter.java
+++ src/PdfConverter.java
@@ -1,20 +1,24 @@
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.StringReader;
 import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.lowagie.text.Document;
-import com.lowagie.text.html.simpleparser.HTMLWorker;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+
 
 public class PdfConverter {
 
@@ -34,11 +38,9 @@ public class PdfConverter {
 	 * environment
 	 * 
 	 */
-
 	public Procedure createProcedure() throws IOException, ParseException {
 
-		FileReader reader = new FileReader(
-				"C:/Users/Sandeep J/Desktop/procedure1.json");
+		FileReader reader = new FileReader("test.json");
 		JSONParser jsonParser = new JSONParser();
 		JSONObject procedureObject = (JSONObject) jsonParser.parse(reader);
 		Procedure procedure = Procedure.JSONToProcedureObject(procedureObject);
@@ -48,151 +50,62 @@ public class PdfConverter {
 	public void createPdf() throws DocumentException, IOException,
 			ParseException, com.lowagie.text.DocumentException {
 		Procedure procedure = createProcedure();
-		OutputStream out = new BufferedOutputStream(
-				new FileOutputStream(new File(
-						"C:/Users/Sandeep J/Desktop/Procedure PDFs/Test2.pdf")));
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(
+				new File("generatedPDF.pdf")));
 		System.out.println("Generating the PDF..");
+
 		Document doc = new Document();
-
-		String str = "";
-		str += "<h1>" + procedure.getName() + "</h1>";
-		HTMLWorker ht = new HTMLWorker(doc);
-
-		PdfWriter.getInstance(doc, new FileOutputStream(new File(
-				"C:/Users/Sandeep J/Desktop/Procedure PDFs/Test2.pdf")));
+		StringBuilder stepString = new StringBuilder();
+		stepString.append("<h1>" + procedure.getName() + "</h1>");
+		FileOutputStream fos = new FileOutputStream(
+				new File("generatedPDF.pdf"));
+		PdfWriter pdfWriter = PdfWriter.getInstance(doc, fos);
+		InputStream is;
 		doc.open();
 		List<Steps> steps = procedure.getSteps();
-		for (Steps step : steps) {
-			str += step.getDetails();
-			str += "<br>";
 
+		for (Steps step : steps) {
+			stepString.append(step.getDetails());
 		}
-		System.out.println(str);
-		ht.parse(new StringReader(str));
+
+		Image img = null;
+		while (stepString.length() > 0) {
+			if (stepString.toString().contains("<img")) {
+				String html = stepString.substring(0,
+						stepString.indexOf("<img"));
+				String restHtml = stepString.substring(stepString
+						.indexOf("<img"));
+				String imageTag = restHtml.substring(restHtml.indexOf("<img"),
+						restHtml.indexOf(".jpeg") + 5);
+				String imgSrc = imageTag.substring(
+						imageTag.indexOf("src=") + 5,
+						imageTag.indexOf(".jpeg") + 5);
+
+				// Initialize the image
+				img = Image.getInstance(imgSrc);
+
+				// Rendering HTML into a pdf
+				is = new ByteArrayInputStream(html.getBytes());
+				XMLWorkerHelper.getInstance().parseXHtml(pdfWriter, doc, is);
+
+				// Adding Image to the PDF in place
+				doc.add(img);
+
+				// Resetting the HTML String to a new point
+				stepString.delete(0, stepString.indexOf(".jpeg") + 5);
+				// Setting Image to null
+				img = null;
+			} else {
+				is = new ByteArrayInputStream(stepString.toString().getBytes());
+				XMLWorkerHelper.getInstance().parseXHtml(pdfWriter, doc, is);
+
+				stepString.delete(0, stepString.length());
+			}
+		}
 		doc.newPage();
 		System.out
 				.println("PDF generation complete. Please check the output folder.");
 		doc.close();
 		out.close();
-
-		// try {
-		//
-		// final FopFactory fopFactory = FopFactory.newInstance(new File(".")
-		// .toURI());
-		// FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-		// // Construct fop with desired output format
-		// Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent,
-		// out);
-		//
-		// // Setup XSLT
-		// TransformerFactory factory = TransformerFactory.newInstance();
-		// Transformer transformer = factory.newTransformer(new StreamSource(
-		// "C:/Users/Sandeep J/Desktop/sampleXslt"));
-		//
-		// List<Steps> steps = procedure.getSteps();
-		// for (Steps step : steps) {
-		// str += step.getDetails();
-		// }
-		// // Setup input for XSLT transformation
-		// Source src = new StreamSource(str);
-		//
-		// // Resulting SAX events (the generated FO) must be piped through to
-		// // FOP
-		// Result res = new SAXResult(fop.getDefaultHandler());
-		// System.out.println("jifhduifhdi");
-		// // Start XSLT transformation and FOP processing
-		// transformer.transform(src, res);
-		// System.out.println("here");
-		// } catch (FOPException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (TransformerConfigurationException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (TransformerException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } finally {
-		// out.close();
-		// }
 	}
 }
-// FOP Test
-
-// FopFactory fopFactory = FopFactory.newInstance(new File(
-// "C:/Users/Sandeep J/Desktop/NewTest.json"));
-//
-// Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
-// TransformerFactory factory = TransformerFactory.newInstance();
-// Transformer transformer = factory.newTransformer();
-// String str = "";
-// List<Steps> steps = procedure.getSteps();
-// for (Steps step : steps) {
-// str += step.getDetails();
-// }
-// Source src = new StreamSource(new StringReader(str));
-// Result res = new SAXResult(fop.getDefaultHandler());
-// transformer.transform(src, res);
-// } catch (SAXException e) {
-// // TODO Auto-generated catch block
-// e.printStackTrace();
-// } catch (TransformerException e) {
-// // TODO Auto-generated catch block
-// e.printStackTrace();
-// } finally {
-// out.close();
-// }
-// FLYING SAUCER Test
-
-// StringBuilder string = new StringBuilder();
-// string.append("<html>");
-// string.append("<head>");
-// System.out.println(procedure.getName());
-// string.append("<h1>" + procedure.getName() + "</h1>");
-// string.append("</head>");
-// string.append("<body>");
-// List<Steps> steps = procedure.getSteps();
-// for (Steps step : steps) {
-// string.append(step.getDetails() + "<br>");
-// string.append("</br>");
-// }
-// string.append("</img>");
-// string.append("</body>");
-// string.append("</html>");
-//
-// ITextRenderer renderer = new ITextRenderer();
-// renderer.setDocumentFromString(string.toString());
-// renderer.layout();
-// renderer.createPDF(file);
-// System.out
-// .println("PDF generation complete. Please check the output folder.");
-// file.close();
-//
-
-// XMLWorker TEST
-
-// String string = new String();
-// string += ("<h1>" + procedure.getName() + "</h1>");
-// Document document = new Document(PageSize.A4, 36, 36, 54, 54);
-// PdfWriter writer = PdfWriter.getInstance(document, file);
-//
-// HeaderFooter event = new HeaderFooter();
-// writer.setBoxSize("art", new Rectangle(36, 54, 559, 788));
-// writer.setPageEvent(event);
-// document.open();
-// List<Steps> steps = procedure.getSteps();
-// for (Steps step : steps) {
-// string += (step.getDetails() + "<br>");
-// }
-// InputStream is = new ByteArrayInputStream(string.toString()
-// .getBytes());
-// XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
-//
-// System.out
-// .println("PDF generation complete. Please check the output folder.");
-// document.close();
-// file.close();
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-
